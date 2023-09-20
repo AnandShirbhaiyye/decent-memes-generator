a5da2c4f56b12812f73e96ad7e8a783f7e22e9e0
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-09-20 19:48:45 +0530

    create img and text result container for meme generator

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useState} from "react";
 import './Meme.css';
 
 function Meme() {
+    const [meme, setMeme] = useState(
+        {
+            topText: "",
+            bottomText: "",
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+        }    
+    );
+
+    function getMemeImg() {
+        
+    }
+
   return (
     <>
             <div className="meme-container">
@@ -19,9 +32,16 @@ function Meme() {
                 />
                 <button
                     className="meme-button"
+                    onClick={getMemeImg}
                 >
                     Get a new meme image
                 </button>
+            </div>
+
+            <div className="result-container">
+                <img src={meme.randomImage}/>
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
             </div>
     </>
   )
