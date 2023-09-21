bc5a80a78d2fc1ebc1f8b5daaed811501a49b415
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-09-21 11:27:05 +0530

    added api integration using axios

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,50 +1,75 @@
-import React from 'react';
-import { useState} from "react";
-import './Meme.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./Meme.css";
 
 function Meme() {
-    const [meme, setMeme] = useState(
-        {
-            topText: "",
-            bottomText: "",
-            randomImage: "http://i.imgflip.com/1bij.jpg",
-        }    
-    );
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-    function getMemeImg() {
-        
+  const [allMemes, setAllMemes] = useState([]);
+
+  useEffect(() => {
+    async function getMemes() {
+      try {
+        const response = await axios.get("https://api.imgflip.com/get_memes");
+        setAllMemes(response.data.data.memes);
+      } catch (error) {
+        console.error("Error fetching memes:", error);
+      }
     }
+
+    getMemes();
+  }, []);
+
+  function getMemeImg() {
+    const url = allMemes[Math.floor(Math.random() * allMemes.length)].url;
+    setMeme((prevMeme) => ({ ...prevMeme, randomImage: url }));
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
 
   return (
     <>
-            <div className="meme-container">
-                <input
-                    type="text"
-                    placeholder="Top text"
-                    className="meme-input"
-                    name="topText"
-                />
-                <input
-                    type="text"
-                    placeholder="Bottom text"
-                    className="meme-input"
-                    name="bottomText"
-                />
-                <button
-                    className="meme-button"
-                    onClick={getMemeImg}
-                >
-                    Get a new meme image
-                </button>
-            </div>
+      <div className="meme-container">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="meme-input"
+          name="topText"
+          onChange={handleChange}
+          value={meme.topText}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="meme-input"
+          name="bottomText"
+          onChange={handleChange}
+          value={meme.bottomText}
+        />
+        <button className="meme-button" onClick={getMemeImg}>
+          Get a new meme image 
+        </button>
+      </div>
 
-            <div className="result-container">
-                <img src={meme.randomImage}/>
-                <h2 className="meme-text top">{meme.topText}</h2>
-                <h2 className="meme-text bottom">{meme.bottomText}</h2>
-            </div>
+      <div className="result-container">
+        <img src={meme.randomImage} alt="random-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default Meme
+export default Meme;
